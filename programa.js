88cb009205ebd1243a62e4a4cd4e797b8be3aba4
andressabogalrodriguez <144062718+andressabogalrodriguez@users.noreply.github.com>
2023-11-10 17:32:08 -0500

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([4.769166667,-74.030277778 ]).addTo(map);
